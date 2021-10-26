40082bf103cd74a0e91cd66edcea6a94d1edcf89
Sergey <82751394+BogdanSS89@users.noreply.github.com>
2021-10-26 18:38:47 +0200

    Add files via upload

--- Task3.cpp
+++ Task3.cpp
@@ -1,11 +1,45 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
+class Figure {
+public:
 
+    virtual void area()=0; 
+    void printArea();
+
+};
+class Parallelogram : public Figure {
+private:
+    int a = 4;
+    int h = 2;
+
+public:
+    void area() {
+       int s = a * h;
+    }
+};
+class Circle : public Figure {
+
+    void area();
+
+};
+class Rectangle : public Parallelogram {
+
+    void area();
+};
+class Square : public Parallelogram {
+
+    void area();
+};
+class Rhombus : public Parallelogram {
+
+    void area();
+};
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    return 0;
 }
+
