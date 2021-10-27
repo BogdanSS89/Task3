6e1de4824d163b7382f9e84eeea62f4cd01f5413
BogdanSS <restoff89@gmail.com>
2021-10-27 17:08:08 +0200

    Task3 complete

--- Task3.cpp
+++ Task3.cpp
@@ -102,7 +102,117 @@ public:
 };
 #pragma endregion
 
+#pragma region Task3
 
+class Fraction
+{
+private:
+    double numerator, denominator;
+public:
+    Fraction(double inputNumerator, double inputDenominator)
+        : numerator(inputNumerator), denominator(inputDenominator)
+    {
+        if (denominator == 0) denominator = 0.00000001;
+    }
+
+    friend Fraction operator+(const Fraction& d1, const Fraction& d2);
+    friend Fraction operator-(const Fraction& d1, const Fraction& d2);
+    friend Fraction operator*(const Fraction& d1, const Fraction& d2);
+    friend Fraction operator/(const Fraction& d1, const Fraction& d2);
+    friend Fraction operator-(const Fraction& d1);
+    friend ostream& operator<<(ostream& out, const Fraction& fraction);
+
+    void setDenominator(double inputDenominator)
+    {
+        if (inputDenominator == 0) inputDenominator = 0.00000001;
+        denominator = inputDenominator;
+    }
+
+    void setNumerator(double inputNumerator)
+    {
+        numerator = inputNumerator;
+    }
+
+    double getNumerator()
+    {
+        return numerator;
+    }
+
+    double getDenominator()
+    {
+        return denominator;
+    }
+};
+
+Fraction operator+(const Fraction& d1, const Fraction& d2)
+{
+    return Fraction(d1.numerator * d2.denominator + d2.numerator * d1.denominator, d1.denominator * d2.denominator);
+}
+
+Fraction operator-(const Fraction& d1, const Fraction& d2)
+{
+    return Fraction(d1.numerator * d2.denominator - d2.numerator * d1.denominator, d1.denominator * d2.denominator);
+}
+
+Fraction operator*(const Fraction& d1, const Fraction& d2)
+{
+    return Fraction(d1.numerator * d2.numerator, d1.denominator * d2.denominator);
+}
+
+Fraction operator/(const Fraction& d1, const Fraction& d2)
+{
+    return Fraction(d1.numerator * d2.denominator, d1.denominator * d2.numerator);
+}
+
+Fraction operator-(const Fraction& d1)
+{
+    return Fraction(-d1.numerator, -d1.denominator);
+}
+
+bool operator==(Fraction& d1, Fraction& d2)
+{
+    if ((d1.getNumerator() == d2.getNumerator()) && (d1.getDenominator() == d2.getDenominator())) return true;
+    else return false;
+}
+
+bool operator!=(Fraction& d1, Fraction& d2)
+{
+    if (d1 == d2) return false;
+    else return true;
+}
+
+bool operator>(Fraction& d1, Fraction& d2)
+{
+    if ((d1.getNumerator() / d1.getDenominator()) > (d2.getNumerator() / d2.getDenominator())) return true;
+    else return false;
+}
+
+bool operator<=(Fraction& d1, Fraction& d2)
+{
+    if (!(d1 > d2)) return true;
+    else return false;
+}
+
+bool operator<(Fraction& d1, Fraction& d2)
+{
+    if ((d1.getNumerator() / d1.getDenominator()) < (d2.getNumerator() / d2.getDenominator())) return true;
+    else return false;
+}
+
+bool operator>=(Fraction& d1, Fraction& d2)
+{
+    if (!(d1 < d2)) return true;
+    else return false;
+}
+
+
+ostream& operator<<(ostream& out, const Fraction& fraction)
+{
+    out << fraction.numerator << "/" << fraction.denominator;
+    return out;
+}
+
+#pragma endregion
 
     int main()
     {
@@ -124,10 +234,27 @@ public:
 
         cout << endl;
 
-        Car* car = new Car("Company", "Model");
         PassengerCar* passengerCar = new PassengerCar("Lada", "Vesta");
         Bus* bus = new Bus("Ikarus", "2406");
         Minivan* minivan = new Minivan("Mersedes", "Vitto");
+
+        cout << endl;
+
+        Fraction d1(6, 4);
+        Fraction d2(5, 8);
+        cout << "\n\n" << (d1 + d2);
+        cout << "\n\n" << (d1 - d2);
+        cout << "\n\n" << (d1 * d2);
+        cout << "\n\n" << (d1 / d2);
+        cout << "\n\n" << (-d1) << "\n";
+
+        if (d1 == d2) cout << "\nFraction 1 == Fraction 2";
+        if (d1 != d2) cout << "\nFraction 1 != Fraction 2";
+        if (d1 > d2) cout << "\nFraction 1 > Fraction 2";
+        if (d1 <= d2) cout << "\nFraction 1 <= Fraction 2";
+        if (d1 < d2) cout << "\nFraction 1 < Fraction 2";
+        if (d1 >= d2) cout << "\nFraction 1 >= Fraction 2";
+    
 
         return 0;
     }
